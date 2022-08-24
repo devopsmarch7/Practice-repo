1ccfe3f42311bd328f81c033b54e5c1e15b5beca
Practicerepo <srmsow2015@gmail.com>
2022-08-24 09:46:31 +0530

    Reverted .. Revert "committing the 2 files created"
    This reverts commit d1fcfc9125a51adfdf8d29b960f9d23e1f749cbe.

--- Deploy.java
+++ Deploy.java
@@ -1,6 +1,3 @@
 1.This file contains deployment details
 
-2.Updating the file with this line.
-
-3. "24-08-22' Bath & Body Works, LLC. is an American retail store chain that sells soaps, lotions, fragrances, and candles. 
-
+2.Updating the file with this line. 
--- test.sh
+++
@@ -1,3 +0,0 @@
-1.This is first line. Bath & Body Works, LLC. is an American retail store chain that sells soaps, lotions, fragrances, and candles.
-
-2.Moved from statging area --> working area and now moving again --> working area
